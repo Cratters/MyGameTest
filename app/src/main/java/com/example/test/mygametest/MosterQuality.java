ff6eb6efed5ca3f4d0e5a9b659d7e1006a9fc9ec
Cratters <4a2k0055@stust.edu.tw>
2017-11-14 01:25:17 +0800

    171114

---
+++ app/src/main/java/com/example/test/mygametest/MosterQuality.java
@@ -0,0 +1,21 @@
+package com.example.test.mygametest;
+
+import android.media.Image;
+
+/**
+ * Created by acer on 2017/11/13.
+ */
+
+public class MosterQuality {
+    int kind;
+    int maxNum;
+    int imgId;
+
+    public MosterQuality(int kind, int maxNum, int imgId)
+    {
+        this.kind = kind;
+        this.maxNum = maxNum;
+        this.imgId = imgId;
+    }
+}
+
